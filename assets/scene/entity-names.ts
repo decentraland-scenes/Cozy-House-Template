281bee04701c3aacf58afb3a48482d7165363240
nearnshaw <earnshaw.nico@gmail.com>
2025-10-17 14:20:36 -0300

    first commit

---
+++ assets/scene/entity-names.ts
@@ -0,0 +1,41 @@
+// Auto-generated entity names from the scene
+
+
+/**
+ * Object containing all entity names in the scene for autocomplete support.
+ */
+export enum EntityNames {
+  Admin_Tools = "Admin Tools",
+  Ambient_Sound___Crickets = "Ambient Sound - Crickets",
+  Ambient_Sound___Fire = "Ambient Sound - Fire",
+  Bookshelf = "Bookshelf",
+  Campfire = "Campfire",
+  Chair_01 = "Chair_01",
+  Chair_01_2 = "Chair_01_2",
+  Chair_02 = "Chair_02",
+  Chair_02_2 = "Chair_02_2",
+  Chimney = "Chimney",
+  Ferns_Yard = "Ferns_Yard",
+  Grass_Yard = "Grass_Yard",
+  House = "House",
+  Image = "Image",
+  Image_Frame = "Image_Frame",
+  Instagram_Link = "Instagram Link",
+  Lamp_Exterior = "Lamp_Exterior",
+  Lamp_Interior = "Lamp_Interior",
+  NFT = "NFT",
+  NFT_Frame = "NFT_Frame",
+  Outside_Fence = "Outside_Fence",
+  Plant_01 = "Plant_01",
+  Plant_02 = "Plant_02",
+  Plant_03 = "Plant_03",
+  Rocks_Front_Door = "Rocks_Front_Door",
+  Rug_Elevator = "Rug_Elevator",
+  Sofa_01 = "Sofa_01",
+  Sofa_02 = "Sofa_02",
+  Table = "Table",
+  Trees_Yard = "Trees_Yard",
+  Twitter_Link = "Twitter Link",
+  Wood_Deck = "Wood_Deck",
+  Wooden_Door = "Wooden Door",
+} 
